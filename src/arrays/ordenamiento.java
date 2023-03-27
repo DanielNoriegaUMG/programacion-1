0d4eb408cfc9a9074572de115958b970e1c2d277
Noriega402 <d.noriegachajon@gmail.com>
2023-03-27 13:39:30 -0600

    ordenamiento de arreglos

---
+++ src/arrays/ordenamiento.java
@@ -0,0 +1,38 @@
+package arrays;
+
+/**
+ *
+ * @author Daniel Noriega
+ */
+public class ordenamiento {
+    public static void main(String[] args) {
+        int[] numeros = {34, 56, 4 , 77, 51, 93, 10, 30, 5, 52};
+        int temporal;
+        
+        System.out.print("Desordenado: ");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i] + " ");
+        }
+        
+        //Ordenamiento de arreglo
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = 0; j < numeros.length - 1; j++) {
+                if(numeros[j] > numeros[j+1]){
+                    temporal = numeros[j];
+                    numeros[j] = numeros[j+1];
+                    numeros[j+1] = temporal;
+                }
+            }
+        }
+        
+        System.out.print("\n\nOrdenado: ");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i] + " ");
+        }
+        
+        System.out.print("\n\nDescendente: ");
+        for (int i = numeros.length - 1; i >= 0; i--) {
+            System.out.print(numeros[i] + " ");
+        }
+    }
+}
